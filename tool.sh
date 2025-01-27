84cac453d8554e9e2d2d291eb17f2afc4c864860
Uros Zigic <98834902+skuller7@users.noreply.github.com>
2025-01-27 16:03:49 +0100

    Nested case statement -  menu - p1.2

--- tool.sh
+++ tool.sh
@@ -28,14 +28,51 @@ sleep 2
 echo "1) Prikaz svih objekata u mrezi"
 echo "2) Prikaz verzije servera"
 echo "3) Prikaz informacija o domenu"
-read -p "Unesite broj opcije:" OPCIJA
+read -p "Unesite broj kategorije:" KATEGORIJA
 
 
-case $OPCIJA in
+case $KATEGORIJA in
 	1)
-		echo "Opcija 1 vrca sve dostupne mreze u JSON dokumentu"
-		ENDPOINT="/api/fmc_config/v1/domain/$DOMAIN_UUID/object/networks"
+		echo "Izabrali ste: Objekti"
+		echo "#######################################"
+		echo "1) Prikaz svih mreznih objekata"
+		echo "2) Prikaz svih mreznih hostova"
+		echo "3) Prikaz svih mreznih grupa"
+		echo "4) Prikaz svih ipv4 addressa"
+		echo "5) Prikaz svih portova"
+		echo "6) Prikaz svih mreznih bezbednosnih zona"
+		echo "7) Prikaz svih dostupnih VLAN-ova unutar mreza"
+		read -p "Unesite broj opcije: " OPCIJA
+		
+		case $OPCIJA in
+			1)
+				ENDPOINT="/api/fmc_config/v1/domain/$DOMAIN_UUID/object/networks"
+				;;
+			2)
+				ENDPOINT="/api/fmc_config/v1/domain/$DOMAIN_UUID/object/hosts"
+				;;
+			3)
+				ENDPOINT="/api/fmc_config/v1/domain/$DOMAIN_UUID/object/networkgroups"
+				;;
+			4)
+				ENDPOINT="/api/fmc_config/v1/domain/$DOMAIN_UUID/object/networkaddresses"
+				;;
+			5)
+				ENDPOINT="/api/fmc_config/v1/domain/$DOMAIN_UUID/object/ports"
+				;;
+			6)
+				ENDPOINT="/api/fmc_config/v1/domain/$DOMAIN_UUID/object/securityzones"
+				;;
+			7) 
+				ENDPOINT="/api/fmc_config/v1/domain/$DOMAIN_UUID/object/vlantags"
+				;;
+			*)
+				echo "Nepoznata opcija, izlazim!"
+				exit 1
+				;;
+		esac
 		;;
+	
 	2)
 		echo "Opcija 2 vraca Verziju i ostale meta-podatke FMC server-a u JSON dokumentu"
 		ENDPOINT="/api/fmc_platform/v1/info/serverversion"
